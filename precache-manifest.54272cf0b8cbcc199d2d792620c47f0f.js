74c3fe4c87179d81cd00081f3b88ef9681c93614
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-06 21:57:11 -0400

    Updates

---
+++ precache-manifest.54272cf0b8cbcc199d2d792620c47f0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692ebfe7cda00ff545a724330b35c416",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "c4999d8dad83200071dd",
+    "url": "/google-books/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da99e750d1b7e447a608",
+    "url": "/google-books/static/js/2.9c08345b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-books/static/js/2.9c08345b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4999d8dad83200071dd",
+    "url": "/google-books/static/js/main.544bbbe5.chunk.js"
+  },
+  {
+    "revision": "be043dc6f38a1fa87439",
+    "url": "/google-books/static/js/runtime-main.68ad9946.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books/static/media/logo.5d5d9eef.svg"
+  }
+]);
