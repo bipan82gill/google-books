47a8f2e0596125937e04708cc47fd560b71e958c
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-14 00:51:33 -0400

    setup styling

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -7,8 +7,8 @@ const BookCard = (props) =>{
       <div className ="card-container">
           <img src={props.image}alt={props.title} />
             <p>{props.title}</p>
-            <h4>Author:{props.author}</h4>
-            <p>Publish Date:{props.published === '0000'? "not Available" : props.published.substring(0,4)}</p>
+            <h5>Author:{props.author}</h5>
+            <p>Publish Data:{props.published === '0000'? "Not Available" : props.published.substring(0,4)}</p>
          
       </div>
   )
